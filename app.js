a9c88d10b0216e978cc6ddecb0a46b1c67832e59
Austin Hines <Austinhines0605@gmail.com>
2021-03-31 10:10:42 -0500

    Finished

--- app.js
+++ app.js
@@ -1,50 +1,92 @@
 const mulan = {
     title: `mulan`,
     main: `Fa Mulan`,
-    quote: function(){
+    quote: function () {
         return `Dishonor! Dishonor on your whole family!`
     },
-    storyLine: function() {
-       return this.title + "&" + this.main
+    storyLine: function () {
+        return `the movie ${this.title} is about ${this.main}`
     }
-}
+};
 
 const tangled = {
     title: `Tangled`,
     main: `Rapunzel`,
-    quote: function() {
+    quote: function () {
         return `I can't believe I did this! I can't believe I did this! I have to go home! I am never going back! I'm a horrible daughter! BEST DAY EVER!`
     },
-    storyLine: function() {
-        return this.title + "&" + this.main
+    storyLine: function () {
+        return `the movie ${this.title} is about ${this.main}`
     }
-}
+};
 
-function DisneyMovie(title, main){
+function DisneyMovie(title, main) {
     this.title = title;
     this.main = main;
 }
 
-// DisneyMovie.prototype.storyLine = function() {
-//     this.main,
-//     this.title
-// }
-
-const mulan1 =  new DisneyMovie(`mulan`, `Fa Mulan`, `Dishonor! Dishonor on your whole family!` )
-
-const tangeld1 = new DisneyMovie(`tangled`, `rapunzel`,`I can't believe I did this! I can't believe I did this! I have to go home! I am never going back! I'm a horrible daughter! BEST DAY EVER!` )
-
-const DM = (title, main) => {
-    this.title = title,
-    this.main = main
+DisneyMovie.prototype.storyLine = function () {
+    return `the movie ${this.title} is about ${this.main}`
 }
-    
-    
+
+const mulan1 = new DisneyMovie(`mulan`, `Fa Mulan`)
+
+const tangeld1 = new DisneyMovie(`tangled`, `rapunzel`)
+
+class DM {
+    constructor(title, main) {
+        this.title = title,
+            this.main = main
+    }
+    storyLine() {
+        `the movie ${this.title} is about ${this.main}`;
+    }
+    static loveDisneyMovies() {
+        return `I love Disney Movies`
+    }
+}
+
+const mulan2 = new DM(`Mulan`, `Fa Mulan`)
+
+const tangeld2 = new DM(`tangled`, `rapunzel`)
+
+class DMCast extends DM {
+    constructor(title, main, cast) {
+        super(title, main);
+        this.cast = cast
+    }
+}
+
+const mulan3 = new DMCast(
+    `Mulan`,
+    `Fa Mulan`,
+    {
+        mulan: `Ming-Na Wen`,
+        mushu: `Eddie Murphy`,
+        shang: `BD Wong`,
+        theEmperor: `Pat Morita`
+    }
+)
+
+const tangeld3 = new DMCast(
+    `tangeld`,
+    `rapunzel`,
+    {
+        rapunzel: `Mandy Moore`,
+         flynnRider: `Zachary Levi`,
+          motherGothel: `Donna Murphy`
+    }
+)
+
+const 
 
 
-// DM.prototype.storyLine  = function(){
-//     this.main
-//     this.title
-// }
 
-const mulan2 = new mulan(`mulan`, `Fa Mulan`,)
+
+
+
+
+
+
+
+
